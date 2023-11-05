bd2c50ee587bd2c6085b76926f71fc88682ed4d9
David Jia <105570809@qq.com>
2023-11-05 12:23:00 +0800

    work on animations

---
+++ lib/views/compoents/animations/small_error_animation_view.dart
@@ -0,0 +1,9 @@
+import 'package:instagram_clone_course/views/compoents/animations/models/lottie_animation.dart';
+import 'package:instagram_clone_course/views/compoents/animations/models/lottie_animation_view.dart';
+
+class SmallErrorAnimationView extends LottieAnimationView {
+  const SmallErrorAnimationView({super.key})
+      : super(
+          animation: LottieAnimation.smallError,
+        );
+}
